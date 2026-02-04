fe420319c17af6ca13bf439b7e62ede25cac1f52
sten-tonis.feldmann <sten-tonis.feldmann@voco.ee>
2026-02-04 10:49:18 +0200

    Klassi loomine

---
+++ oop_simple.py
@@ -0,0 +1,13 @@
+"""Simple class."""
+
+
+class Student:
+    """Making a class named Student."""
+
+    def __init__(self, name):
+        """Class named Student."""
+        self.name = name
+        self.finished = False
+
+
+student1 = Student("John")
